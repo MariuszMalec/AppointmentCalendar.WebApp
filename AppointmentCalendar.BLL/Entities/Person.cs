f4943282b337a9af15a8640ab8bcee21a904a356
Mariusz Malec <mario106c@gmail.com>
2022-07-04 17:15:41 +0200

    added new propertieces to model person

--- AppointmentCalendar.BLL/Entities/Person.cs
+++ AppointmentCalendar.BLL/Entities/Person.cs
@@ -11,5 +11,15 @@ namespace AppointmentCalendar.BLL.Entities
         public string FirstName { get; set; }
 
         public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+
+        public string AddressCorrespondence { get; set; }
+
+        public int PhoneNumber { get; set; }
+
+        public string PhoneMobile { get; set; }
     }
 }
