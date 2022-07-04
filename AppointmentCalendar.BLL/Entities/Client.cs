2ce6b6e5fdf444f504ed3da546c3e524e88d6d24
Mariusz Malec <mario106c@gmail.com>
2022-07-04 17:25:59 +0200

    added client model

---
+++ AppointmentCalendar.BLL/Entities/Client.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentCalendar.BLL.Entities
+{
+    public class Client : Person
+    {
+
+    }
+}
